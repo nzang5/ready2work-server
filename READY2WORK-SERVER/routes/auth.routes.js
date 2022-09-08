88c7a0c1390bb83bcda1f683a965d203605d69f6
Lorena Garcia <lorena_65_11@hotmail.com>
2022-09-08 11:20:30 +0200

    first pull

---
+++ READY2WORK-SERVER/routes/auth.routes.js
@@ -0,0 +1,127 @@
+const express = require("express");
+const bcrypt = require('bcryptjs'); // To encrypt the passwords
+const jwt = require("jsonwebtoken"); // To create TOKENS
+const User = require("../models/User.model");
+ 
+const router = express.Router();
+const saltRounds = 10;
+
+//const { isAuthenticated } = require('./../middleware/jwt.middleware.js');
+
+ 
+ 
+// POST  /auth/signup
+router.post('/signup', (req, res) => {
+    const { email, password, name } = req.body;
+   
+    // To ensure all fields are mandatory
+    if (email === '' || password === '' || name === '') {
+      res.status(400).json({ message: "Provide email, password and name" });
+      return;
+    }
+   
+    // With regex we ensure to validate the email format
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]{2,}$/;
+    if (!emailRegex.test(email)) {
+      res.status(400).json({ message: 'Provide a valid email address.' });
+      return;
+    }
+    
+    // Use regex to validate the password format
+    const passwordRegex = /(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,}/;
+    if (!passwordRegex.test(password)) {
+      res.status(400).json({ message: 'Password must have at least 6 characters and contain at least one number, one lowercase and one uppercase letter.' });
+      return;
+    }
+   
+   
+    // To make sure emails are unique
+    User.findOne({ email })
+      .then((foundUser) => {
+        if (foundUser) {
+          res.status(400).json({ message: "This User already exists." });
+          return;
+        }
+        //Hashing password
+        const salt = bcrypt.genSaltSync(saltRounds);
+        const hashedPassword = bcrypt.hashSync(password, salt); 
+   
+        // New user in the db
+        return User.create({ email, password: hashedPassword, name });
+      })
+      .then((createdUser) => {
+        // We pass user info to front end minus password
+        const { email, name, _id } = createdUser;
+
+        const user = { email, name, _id };
+   
+        // Here we pass the user to front end as .json
+        res.status(201).json({ user: user });
+      })
+      .catch(err => {
+        console.log(err);
+        res.status(500).json({ message: "Internal Server Error" })
+      });
+  });
+ 
+ 
+// POST  /auth/login
+router.post('/login', (req, res) => {
+    const { email, password } = req.body;
+   
+    // Check if email or password are provided as empty string 
+    if (email === '' || password === '') {
+      res.status(400).json({ message: "Provide email and password." });
+      return;
+    }
+   
+    // Check the users collection if a user with the same email exists
+    User.findOne({ email })
+      .then((foundUser) => {
+      
+        if (!foundUser) {
+          // If the user is not found, send an error response
+          res.status(401).json({ message: "User not found." })
+          return;
+        }
+   
+        // Compare the provided password with the one saved in the database
+        const passwordCorrect = bcrypt.compareSync(password, foundUser.password);
+   
+        if (passwordCorrect) {
+          // Deconstruct the user object to omit the password
+          const { _id, email, name } = foundUser;
+          
+          // Create an object that will be set as the token payload
+          const payload = { _id, email, name };
+   
+          // Create and sign the token
+          const authToken = jwt.sign( 
+            payload,
+            process.env.TOKEN_SECRET,
+            { algorithm: 'HS256', expiresIn: "6h" }
+          );
+   
+          // Send the token as the response
+          res.status(200).json({ authToken: authToken });
+        }
+        else {
+          res.status(401).json({ message: "Password is incorrect. Unable to authenticate the user" });
+        }
+   
+      })
+      .catch(err => res.status(500).json({ message: "Internal Server Error" }));
+  });
+ 
+ 
+// GET  /auth/verify
+/*router.get('/verify', isAuthenticated, (req, res) => {
+ 
+    // If JWT token is valid the payload gets decoded by the
+    // isAuthenticated middleware and made available on `req.payload`
+    console.log(`req.payload`, req.payload);
+  
+    res.status(200).json(req.payload); // Send back the object with user data
+  });*/
+ 
+module.exports = router;
