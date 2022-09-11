3849a828f4a42c7923701d3a412bbcacd492a441
Nash Zangio <nash.c.zangio@gmail.com>
2022-09-11 19:43:17 +0200

    edit/delete routes

--- READY2WORK-SERVER/routes/JobPost.routes.js
+++ READY2WORK-SERVER/routes/JobPost.routes.js
@@ -12,6 +12,39 @@ router.get('/jobsList', (req, res) =>{
 
 })
 
+// GET - Get a specific job by id
+router.get('/jobslist/:jobId', (req, res, next) => {
+    const { jobId } = req.params;
+    console.log(jobId)
+    JobPost.findById(jobId)
+    .then(results => {
+        res.status(200).json(results)
+    })
+    .catch(error => { console.log(error)})
+    
+});
+
+// PUT  /api/jobslist/:jobsId  -  Updates a specific jobs by id
+router.put('/jobslist/:jobId', (req, res, next) => {
+    const { jobId } = req.params;
+
+    JobPost.findByIdAndUpdate(jobId, req.body, { new: true })
+    .then((updatedJobPost) => res.json(updatedJobPost))
+    .catch(error => res.json(error));
+});
+
+// DELETE  /api/jobslist/:jobsId  -  Deletes a specific jobs by id
+router.delete('/jobslist/:jobId', (req, res, next) => {
+    const { jobId } = req.params;
+
+    JobPost.findByIdAndRemove(jobId)
+    .then(() => res.json({ message: `Project with ${jobsId} is removed successfully.` }))
+    .catch(error => res.json(error));
+});
+
+
+
+
 router.post('/create', (req, res) => {
     console.log("made it here")
     const { title, description, companyName } = req.body;
